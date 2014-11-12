7c612422890f9f30f6eaba9cbf452581864402b9
Rafael Silla <rafelver@gmail.com>
2014-11-12 07:48:10 +0100

    proyecto 01 con jdk 1.8
    Signed-off-by: Rafael Silla <rafelver@gmail.com>

---
+++ ConversorDolares2/src/ConversorDolares.java
@@ -0,0 +1,11 @@
+public class ConversorDolares {
+      
+	public static void main(String[] args) {
+		float valorConversion;
+		valorConversion=1.25f;
+		VentanaConversor frame = new VentanaConversor(valorConversion);
+		frame.setVisible(true);
+
+	}
+
+}
